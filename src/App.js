8da007cf8e765ad57bb7b146aaf5cd8e6ae35de7
Praveen Kumar Ramineni <pramineni01@gmail.com>
2021-11-27 19:46:01 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from "react";
+import "./styles.css";
+
+// khobarcardiff1234
+
+export default function App() {
+  return (
+    <div className={"Page"}>
+      <div className={"PageHeader"}>
+        <div className={"PageTitle"}>Hello CodeSandbox</div>
+        <div className={"PageSubTitle"}>
+          Start editing to see some magic happen!
+        </div>
+      </div>
+
+      <div className="PageBody">
+        <div className={"PageSectionTitle"}>This is a heading</div>
+        <div className={"PageText"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className={"PageSectionTitle"}>This is a heading</div>
+        <div className={"PageText"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className={"PageSectionTitle"}>This is a heading</div>
+        <div className={"PageText"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+        <div className={"PageSectionTitle"}>This is a heading</div>
+        <div className={"PageText"}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </div>
+      </div>
+    </div>
+  );
+}
